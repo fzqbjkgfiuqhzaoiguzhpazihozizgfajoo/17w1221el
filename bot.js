3700e7bb2880f17dfb340deb15d098fdd912a989
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-01 11:38:44 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,19 @@ const client = new Discord.Client();
 const Discord1 = require("discord.js");
 const client1 = new Discord1.Client();
 
+clien1.on('ready' , () => {
+    console.log('Online.');
+});
+
+
+
+client1.on('ready' , () => {
+    console.log('Online.');
+});
+
+
+
+
 
 client.on("guildMemberAdd", (member) => {
         var guild = client.guilds.find("name", 'Oreo Area ,');
@@ -14,7 +27,7 @@ const channel = guild.channels.find(channel => channel.name == 'oreo');
 
 
 client1.on("guildMemberAdd", (member) => {
-        var guild = client1l.guilds.find("name", 'Oreo Area ,');
+        var guild = client1.guilds.find("name", 'Oreo Area ,');
 const channel = guild.channels.find(channel => channel.name == 'oreo');
          channel.send(`** # Welcome To Our Server :rose: :3_:  **`);
 });
