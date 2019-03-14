defecc019438c1a515c1ec3c671ea845eb578ae3
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2019-03-14 08:43:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,23 +19,23 @@ client1.on('ready' , () => {
 
 
 client.on("guildMemberAdd", (member) => {
-        var guild = client.guilds.find("name", 'Oreo Area ,');
-const channel = guild.channels.find(channel => channel.name == 'oreo');
-         channel.send(`** # Welcome To Our Server :rose: .. **`);
+        var guild = client.guilds.find("name", ', Berlin ' Community .');
+const channel = guild.channels.find(channel => channel.name == 'berlin');
+         channel.send(`** Wel__come__ To `` # `` __Ber__lin :flag_de: **`);
 });
 
 
 
 client1.on("guildMemberAdd", (member) => {
-        var guild = client1.guilds.find("name", 'Oreo Area ,');
-const channel = guild.channels.find(channel => channel.name == 'oreo');
-         channel.send(`** # Welcome To Our Server :rose: .. **`);
+        var guild = client1.guilds.find("name", ', Berlin ' Community .');
+const channel = guild.channels.find(channel => channel.name == 'berlin');
+         channel.send(`** Wel__come__ To `` # `` __Ber__lin :flag_de: **`);
 });
 
 
 client.on('message', message => {
-    var ownerid = '455331653309562910';
-    var prefix = '17';
+    var ownerid = '541614978319384597';
+    var prefix = 'B';
     let args = message.content.split(' ').slice(1)
     if (message.author.id != ownerid) return;
     if (message.content.startsWith(prefix + 'l')) {
@@ -61,6 +61,33 @@ client.on('message', message => {
     }
 });
 
+client1.on('message', message => {
+    var ownerid = '541614978319384597';
+    var prefix = 'B';
+    let args = message.content.split(' ').slice(1)
+    if (message.author.id != ownerid) return;
+    if (message.content.startsWith(prefix + 'l')) {
+        if (message.author.id != ownerid) return;
+        else {
+            client.user.setActivity(args.join(' '), {
+                type: 'LISTENING'
+            });
+            message.channel.send(`My New Listening is = ${args.join('  ')}`)
+
+        }
+    }
+    if (message.author.id != ownerid) return;
+    if (message.content.startsWith(prefix + 'w')) {
+        if (message.author.id != ownerid) return;
+        else {
+            client1.user.setActivity(args.join(' '), {
+                type: 'WATCHING'
+            });
+            message.channel.send(`My New Watching is = ${args.join('  ')}`)
+
+        }
+    }
+});
 client.login(process.env.BOT_TOKEN);
 
 client1.login(process.env.BOT1_TOKEN);
